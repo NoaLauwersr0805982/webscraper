5e0f7afe834b113ba7a00bafb5ac9cbd1ded82ca
NoaLauwersr0805982 <89747596+NoaLauwersr0805982@users.noreply.github.com>
2021-08-29 21:41:28 +0200

    Add files via upload

---
+++ Scraper.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd 
+import time as t
+import redis as redis
+
+
+def RedisCatcher(elemlist1):
+    scraper_df = pd.DataFrame(elemlist1, columns=['Hash', 'Time', 'Bitcoin', 'US Dollar'])
+
+    red = redis.Redis()
+
+    red.set('scraperdf', scraper_df.to_json())
+    
+
+
+def ElementsCatcher(Elements):
+
+    elemlist1 = []
+
+    for Element in Elements:
+
+        Hash = Element.find('a')
+
+        elemlist2 = []
+        elemlist2.append(Hash.text)
+
+        TiBitDollar = Element.find_all('span')
+        
+        nottext = ('Hash', 'Time', 'Amount (BTC)', 'Amount (USD)')
+
+        for data in TiBitDollar:
+            if (data.text not in nottext):
+                elemlist2.append(data.text)
+
+        elemlist2[3] = elemlist2[3].replace(',', '')
+        elemlist2[3] = elemlist2[3].replace('$', '')
+        elemlist2[3] = float(elemlist2[3])
+
+        elemlist1.append(elemlist2)
+
+        RedisCatcher(elemlist1)
+
+    print(elemlist1)
+
+
+def MainScraper():
+    url = 'https://www.blockchain.com/btc/unconfirmed-transactions'
+
+    request = requests.get(url)
+
+    soup = BeautifulSoup(request.text, 'html.parser')
+
+    Elements = soup.find_all('div', class_='hXyplo')
+    ElementsCatcher(Elements)
+
+MainScraper()
+
+while True:
+
+    MainScraper()
+    t.sleep(60)
---
+++ mongodb.py
@@ -0,0 +1,35 @@
+import redis
+import pandas as pd
+from pymongo import MongoClient
+import time as t
+
+
+def GetRedis():
+    red = redis.Redis()
+    redi = red.get('scraperdf')
+    redii = pd.read_json(redi)
+    rediii = redii.sort_values(by = ['US Dollar'], ascending = False).head(1)
+    rediiii = rediii.to_dict('records') #waardes en titles
+    red.flushdb
+    return rediiii
+
+def MainMongo():
+    try:
+        connection = MongoClient('localhost', 27017)
+        print("Connected")
+    except:
+        print("Could not connect to MongoDB")
+
+    db = connection.scraper
+    collect = db.scrapery
+
+    scrapero = GetRedis()
+    collect.insert_many(scrapero)
+
+MainMongo()
+        
+while True:
+    
+    MainMongo()
+    t.sleep(60)
+
